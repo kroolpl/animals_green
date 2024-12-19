1b0e9d4845cab770890b34d4e205bfb06003f139
kroolpl <ggglondyn@gmail.com>
2024-12-19 23:32:58 +0100

    porady fix?

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  type: 'content',
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    excerpt: z.string(),
-    category: z.string(),
-    readTime: z.string(),
-    date: z.date(),
-    image: z.enum(['polki', 'dogFood']),
-  }),
-});
+const validImageKeys = ['dog-food', 'polki', '2psy', 'pet-store-1'] as const;
 
-export const collections = { blog }; 
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      excerpt: z.string(),
+      date: z.date(),
+      author: z.string(),
+      image: z.enum(validImageKeys),
+      category: z.string(),
+      readTime: z.string()
+    })
+  })
+}; 
